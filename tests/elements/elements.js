09a3a88e95e662415d6418717d5b7aceb1bd36d2
Kristine Pantoja <kristine.palafox@gmail.com>
2023-03-29 13:44:01 -0500

    no message

---
+++ tests/elements/elements.js
@@ -0,0 +1,30 @@
+const products = {
+    productsLink : "navbar-products",
+    productFilterText : '.filter-textbox',
+    productFilterBtn : 'filter-button',
+    resetFilterBtn : 'reset-filter-button',
+    addProductBtn : 'add-a-product-button',
+    idCell : 'id',
+    nameCell : 'name',
+    priceCell : 'price',
+    dateStockedCell : 'dateStocked',
+    productListTable : '.product-list-table'
+}
+
+const addProduct = {
+    addProductsLink : "navbar-addproduct"
+}
+
+const practice = {
+    practiceLink : "navbar-practice"
+}
+
+const learn = {
+    learnLink : "navbar-learn"
+}
+
+const login = {
+    loginLink : "navbar-login"
+}
+
+module.exports = { products, learn, login, practice, addProduct };
